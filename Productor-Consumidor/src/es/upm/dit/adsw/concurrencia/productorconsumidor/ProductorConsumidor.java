1f99058f424bf79e1b6c9e0428ab1284be786480
Juan A. de la Puente <jpuente@dit.upm.es>
2012-03-22 10:12:52 +0100

    Cambios menores

--- Productor-Consumidor/src/es/upm/dit/adsw/concurrencia/productorconsumidor/ProductorConsumidor.java
+++ Productor-Consumidor/src/es/upm/dit/adsw/concurrencia/productorconsumidor/ProductorConsumidor.java
@@ -9,8 +9,10 @@ public class ProductorConsumidor {
 	public static void main(String[] args) {
 		Buffer<String> buffer = new BufferMultiple<String>(10);
 		Productor productor1 = new Productor("p1", buffer, 1000);
+		Productor productor2 = new Productor("p2", buffer, 2000);
 		Consumidor consumidor = new Consumidor("c", buffer);
 		productor1.start();
+		productor2.start();
 		consumidor.start();
 	}
 }
